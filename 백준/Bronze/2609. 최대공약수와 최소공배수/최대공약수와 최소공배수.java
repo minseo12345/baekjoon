111bc958061011caf05c030c0e16b1c93bd6f598
minseo <115347424+minseo12345@users.noreply.github.com>
2024-12-04 23:29:26 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = findGCD(a, b);
+        int lcm = (a * b) / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}
