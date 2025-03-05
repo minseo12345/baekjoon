a05da8e5b3b9118b2989cedd05b6077ff94b7077
minseo <115347424+minseo12345@users.noreply.github.com>
2025-03-05 19:03:28 +0900

    [Silver III] Title: N과 M (5), Time: 368 ms, Memory: 32020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] numbers, arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        numbers = new int[N];
+        arr = new int[M];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = numbers[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
