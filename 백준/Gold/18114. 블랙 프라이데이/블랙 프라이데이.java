c8e5841eb4ff29cb56584f4e0ceccffdba12913b
minseo <115347424+minseo12345@users.noreply.github.com>
2025-05-19 16:34:46 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 192 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -1,56 +1,71 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, C;
-    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        C = sc.nextInt();
-        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-        
-        for (int i = 0; i < N; i++) {
 
-            if (arr[i] == C) {
+        int left = 0;
+        int right = N-1;
+
+        // 하나만 고르는 경우
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == C){
                 System.out.println(1);
                 return;
             }
+        }
 
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] + arr[j] == C) {
+        // 2개만 고르는 경우
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if(sum == C){
+                System.out.println(1);
+                return;
+            }else if (sum < C){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        
+        // 3개 고르는 경우
+        for(int i = 0; i<arr.length; i++){
+            int a = arr[i];
+            int l = i+1;
+            int r = N - 1;
+
+            while (l<r) {
+                int sum = a + arr[l] + arr[r] ;
+               if(sum == C) {
                     System.out.println(1);
                     return;
-                }
-                
-                int target = C - (arr[i] + arr[j]);
-                if (binarySearch(arr, j + 1, N - 1, target)) {
-                    System.out.println(1);
-                    return;
-                }
+               }else if(sum < C){
+                    l++;
+               }else{
+                    r--;
+               }
             }
         }
 
         System.out.println(0);
-    }
-    
-    static boolean binarySearch(int[] arr, int start, int end, int target) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] == target) {
-                return true;
-            } else if (arr[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return false;
     }
 }
