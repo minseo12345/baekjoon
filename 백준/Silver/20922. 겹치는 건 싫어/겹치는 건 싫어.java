35bafff5aaacfafb1277fbde8e0cb9e7e147a1b4
minseo <115347424+minseo12345@users.noreply.github.com>
2025-05-25 23:32:21 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 356 ms, Memory: 32916 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] count = new int[100001]; 
+        int left = 0, right = 0;
+        int maxLen = 0;
+
+        while (right < N) {
+            int val = arr[right];
+            count[val]++;
+
+            while (count[val] > K) {
+                count[arr[left]]--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+
+        System.out.println(maxLen);
+    }
+}
